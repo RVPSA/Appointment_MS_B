ee9016e20a550b89896675fd8af290e3ee58b79b
RVPSA <akeshramasinghe@gmail.com>
2025-02-19 19:27:36 +0100

    Modifications to load common configurations from appsettings file and the environment variables

--- User_service/Program.cs
+++ User_service/Program.cs
@@ -1,3 +1,4 @@
+using DotNetEnv;
 using User_service.Common;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,8 +13,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    Env.Load();
     app.MapOpenApi();
 }
+//Load Environment variables
+builder.Configuration.AddEnvironmentVariables();
+//Call Configuration method
+LoadConfiguration();
 
 //Use DirectAccessProtectionMiddleware
 app.UseMiddleware<DirectAccessProtectionMiddleware>();
@@ -21,3 +27,27 @@ app.UseMiddleware<DirectAccessProtectionMiddleware>();
 app.MapControllers();
 
 app.Run();
+
+//Load Configurations from appSettings file
+void LoadConfiguration()
+{
+    AppSettings.JwtSecretKey = Environment.GetEnvironmentVariable("JWT_SECRET");
+    AppSettings.JwtTokenExpiration = Convert.ToDouble(builder.Configuration.GetSection("AuthenticationSettings:TokenExpiration").Value);
+    
+    AppSettings.CookieExpires = Convert.ToDouble(builder.Configuration.GetSection("Cookies:Expires").Value);
+    AppSettings.CookieDomain = builder.Configuration.GetSection("Cookies:Domain").Value;
+    AppSettings.CookiePath = builder.Configuration.GetSection("Cookies:Path").Value;
+    AppSettings.CookieName = builder.Configuration.GetSection("Cookies:CookieName").Value;
+    
+    AppSettings.GatewaySecretKey = Environment.GetEnvironmentVariable("GATEWAY_SECRET");
+    
+    AppSettings.TimeStampHeaderKey = builder.Configuration.GetSection("HeadersList:TimeStampHeaderKey").Value;
+    AppSettings.SignatureHeaderKey = builder.Configuration.GetSection("HeadersList:SignatureHeaderKey").Value;
+    AppSettings.UserIdKey = builder.Configuration.GetSection("HeadersList:UserIdKey").Value;
+    AppSettings.UserRoleKey = builder.Configuration.GetSection("HeadersList:UserRoleKey").Value;
+    
+    AppSettings.ClaimsUserId = builder.Configuration.GetSection("Claims:ClaimsUserId").Value;
+    AppSettings.ClaimUserRole = builder.Configuration.GetSection("Claims:ClaimUserRole").Value;
+    AppSettings.ClaimUserName = builder.Configuration.GetSection("Claims:ClaimUserName").Value;
+    
+}
