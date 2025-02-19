688b1f8c4e19fa3282162f1d4bcd44a07b86d935
RVPSA <akeshramasinghe@gmail.com>
2025-02-19 14:57:17 +0100

    Modify to add gateway secret credentials for all requests that are going through the gateway, used secret key and a hash value generating method

--- Appointment_MS/JwtAuthenticationMiddleware.cs
+++ Appointment_MS/JwtAuthenticationMiddleware.cs
@@ -1,4 +1,5 @@
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Cryptography;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
 
@@ -7,17 +8,15 @@ namespace Appointment_MS;
 public class JwtAuthenticationMiddleware
 {
     private readonly RequestDelegate _next;
-    private readonly IConfiguration _config;
 
     private readonly List<string> _publicRoutes = new()
     {
-        "/api/user/login"
+        "/api/user/login" //TODO Need to load from appsettings
     };
 
-    public JwtAuthenticationMiddleware(RequestDelegate next, IConfiguration config)
+    public JwtAuthenticationMiddleware(RequestDelegate next)
     {
         _next = next;
-        _config = config;
     }
 
     public async Task Invoke(HttpContext context)
@@ -25,6 +24,7 @@ public class JwtAuthenticationMiddleware
         // Allow public routes to pass through without authentication
         if (_publicRoutes.Contains(context.Request.Path.Value.ToLower()))
         {
+            EncryptGatewaySecret(context); //Add GatewaySecret to the request header
             await _next(context);
             return;
         }
@@ -52,6 +52,7 @@ public class JwtAuthenticationMiddleware
             var claims = claimsPrincipal.Claims;
             context.Request.Headers["UserId"] = claims.FirstOrDefault(c => c.Type == "UserId")?.Value ?? "";
             context.Request.Headers["UserRole"] = claims.FirstOrDefault(c => c.Type == "role")?.Value ?? "";
+            EncryptGatewaySecret(context); //Add GatewaySecret to the request header
         }
         catch
         {
@@ -60,5 +61,23 @@ public class JwtAuthenticationMiddleware
         }
 
         await _next(context);
+    }
+
+    private void EncryptGatewaySecret(HttpContext context)
+    {
+        string secretKey = "Application-Management-1234567890"; //TODO Need to load from the appsettings file
+        
+        string timeStamp = DateTime.UtcNow.ToString("o");
+        
+        string data = timeStamp;
+        using (var hmac = new HMACSHA256(Encoding.UTF8.GetBytes(secretKey)))
+        {
+            byte[] hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(data));
+            string signature =  Convert.ToBase64String(hash);
+            
+            //Add timestamp and the signature into the header
+            context.Request.Headers["X-Gateway-Timestamp"] = timeStamp; //TODO Need to load from appsettings
+            context.Request.Headers["X-Gateway-Signature"] = signature; //TODO Need to load from appsettings
+        }
     }
 }
