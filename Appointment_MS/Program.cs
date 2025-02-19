154bcd433e17d435c3fed2eb0ee935bc9a8ec853
RVPSA <akeshramasinghe@gmail.com>
2025-02-19 10:53:07 +0100

    load ocelot json file and use it, use JwtAuthentication custom middleware

--- Appointment_MS/Program.cs
+++ Appointment_MS/Program.cs
@@ -1,8 +1,18 @@
+using Appointment_MS;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Add Ocelot json configuration file
+builder.Configuration.AddJsonFile("Ocelot.json",optional:false,reloadOnChange:true);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+//Add Ocelot api gateway service
+builder.Services.AddOcelot();
 
 var app = builder.Build();
 
@@ -12,28 +22,11 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+//Use JwtAuthenticationMiddleware as a middleware in gateway
+app.UseMiddleware<JwtAuthenticationMiddleware>();
 
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+//Use ocelot
+app.UseOcelot().Wait();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
