b567caddcd0cfbad3e11f959dc4f8bf16202fdd3
RVPSA <akeshramasinghe@gmail.com>
2025-02-19 11:17:36 +0100

    Login controller, that consists login endpoint, call method for token creation if user has given correct credentials and append the cookie for the response

---
+++ User_service/Controllers/LoginController.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Mvc;
+using User_service.ApplicationServices.IServices;
+using User_service.ApplicationServices.Services;
+using User_service.BusinessObjects.Login;
+using User_service.Common;
+
+namespace User_service.Controllers;
+
+[Route("[Controller]/[action]")]
+public class LoginController : Controller
+{
+    [HttpGet]
+    public string LoginTest()
+    {
+        return "Login Test success";
+    }
+    
+    [HttpPost]
+    public string Login([FromBody] LoginUserRequest loginUserRequest)
+    {
+        ILoginService service = new LoginService();
+        try
+        {
+            var result = service.Login(loginUserRequest);
+
+            if (result != null)
+            {
+                var token = new JwtTokenCreator().CreateJwtToken(result);
+                CreateCookie(token);
+
+                return "Logged in success"; //TODO Common Response message
+            }
+
+            return "Logged in failed"; //TODO Common Response message
+        }
+        catch (Exception ex)
+        {
+            return ex.Message; //TODO Common Response Message
+        }
+    }
+    
+    private void CreateCookie(string token)
+    {
+        var options = new CookieOptions();
+        options.Expires = DateTime.Now.AddMinutes(20); //TODO Need to add this for appsettings file
+        options.HttpOnly = true;
+        options.Path = "/";
+        options.Secure = true;
+        options.SameSite = SameSiteMode.None;
+        options.Domain = "localhost";
+        Response.Cookies.Append("token", token, options); //TODO Need to add for appsettings file
+    }
+}
