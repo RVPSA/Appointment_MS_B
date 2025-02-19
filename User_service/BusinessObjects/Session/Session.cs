0634e7ca05d78f4dda0e600f7cc81c3dae1a3c95
RVPSA <akeshramasinghe@gmail.com>
2025-02-19 22:38:25 +0100

    create a class for session details

---
+++ User_service/BusinessObjects/Session/Session.cs
@@ -0,0 +1,7 @@
+namespace User_service.BusinessObjects.Session;
+
+public class Session
+{
+    public string UserId { get; set; } = string.Empty;
+    public string UserRole { get; set; } = string.Empty;
+}
