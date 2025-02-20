e50dbb2930f142551d8df41fb17bba0f54773892
RVPSA <akeshramasinghe@gmail.com>
2025-02-21 00:32:49 +0100

    Create dataservice to add data to the database and get back from it

--- User_service/DAL/DService/LoginDataService.cs
+++ User_service/DAL/DService/LoginDataService.cs
@@ -35,4 +35,62 @@ public class LoginDataService(IDataService dataService) : ILoginDataService
             throw;
         }
     }
+
+    public SignedUpUser? SignUp(SignUpRequest signUpRequest)
+    {
+        try
+        {
+            DbParameter[] dbParameters = new DbParameter[12];
+            
+            dbParameters[0] = DataServiceCreator.CreateDbParameter("@firstname",System.Data.DbType.String,
+                System.Data.ParameterDirection.Input,signUpRequest.FirstName);
+            dbParameters[1] = DataServiceCreator.CreateDbParameter("@lastname",System.Data.DbType.String,
+                System.Data.ParameterDirection.Input,signUpRequest.LastName);
+            dbParameters[2] = DataServiceCreator.CreateDbParameter("@surename",System.Data.DbType.String,
+                System.Data.ParameterDirection.Input,signUpRequest.SureName);
+            dbParameters[3] = DataServiceCreator.CreateDbParameter("@address",System.Data.DbType.String,
+                System.Data.ParameterDirection.Input,signUpRequest.Address);
+            dbParameters[4] = DataServiceCreator.CreateDbParameter("@email",System.Data.DbType.String,
+                System.Data.ParameterDirection.Input,signUpRequest.Email);
+            dbParameters[5] = DataServiceCreator.CreateDbParameter("@role",System.Data.DbType.Int32,
+                System.Data.ParameterDirection.Input,2); //TODO Need to add enum
+            dbParameters[6] = DataServiceCreator.CreateDbParameter("@password",System.Data.DbType.String,
+                System.Data.ParameterDirection.Input,signUpRequest.Password);
+            dbParameters[7] = DataServiceCreator.CreateDbParameter("@contactnumber",System.Data.DbType.String,
+                System.Data.ParameterDirection.Input,signUpRequest.ContactNumber);
+            dbParameters[8] = DataServiceCreator.CreateDbParameter("@dob",System.Data.DbType.Date,
+                System.Data.ParameterDirection.Input,signUpRequest.Dob);
+            dbParameters[9] = DataServiceCreator.CreateDbParameter("@gender",System.Data.DbType.String,
+                System.Data.ParameterDirection.Input,signUpRequest.Gender);
+            dbParameters[10] = DataServiceCreator.CreateDbParameter("@nicnumber",System.Data.DbType.String,
+                System.Data.ParameterDirection.Input,signUpRequest.NicNumber);
+            dbParameters[11] = DataServiceCreator.CreateDbParameter("@status",System.Data.DbType.Int32,
+                System.Data.ParameterDirection.Input,1); //TODO Need to add Enum
+            
+            DbDataReader dbDataReader = dataService.ExecuteReader("login.AddUserPatient",dbParameters,null);
+            
+            if (!dbDataReader.HasRows) return null;
+            while (dbDataReader.Read())
+            {
+                DataReader dataReader = new DataReader(dbDataReader);
+                return new SignedUpUser()
+                {
+                    LastName = dataReader.GetString("lastname"),
+                    UserName = dataReader.GetString("email"),
+                    Email = dataReader.GetString("email"),
+                    ContactNumber = dataReader.GetString("contactnumber"),
+                    NicNumber = dataReader.GetString("nicnumber"),
+                    Role = dataReader.GetString("role"),
+                    PatientNumber = dataReader.GetString("number")
+                };
+            }
+
+            return null;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }  
+    }
 }
--- User_service/DAL/IDService/ILoginDataService.cs
+++ User_service/DAL/IDService/ILoginDataService.cs
@@ -5,4 +5,5 @@ namespace User_service.DAL.IDService;
 public interface ILoginDataService
 {
     public LoggedUser? Login(LoginUserRequest loginUserRequest);
+    public SignedUpUser? SignUp(SignUpRequest signUpRequest);
 }
